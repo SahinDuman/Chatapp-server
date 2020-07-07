d9e74c8deb672d450466dd761a5ca378a39965ac
SahinDuman <sahinduman247@gmail.com>
2020-07-07 18:29:00 +0200

    uncommented the / route

--- src/router.ts
+++ src/router.ts
@@ -1,23 +1,23 @@
-import express, { Request, Response} from 'express';
+import express, { Request, Response } from 'express';
 import { addUser, nameExists, validateName } from './users';
 import { logger } from './utils/logger'
 const router = express.Router();
 
-/* router.get("/", (req: Request, res:Response) => {
+router.get("/", (req: Request, res: Response) => {
   res.send({ response: "Server is up and running." }).status(200);
 });
- */
 
-router.post("/register", (req: Request, res:Response) => {
+
+router.post("/register", (req: Request, res: Response) => {
   const name = req.body.name;
   const nameValid = validateName(name);
   const doesNameExist = nameExists(name);
   logger.info('RECIEVED A POST REQUEST FROM /register', req.body);
-  
-  if(!nameValid || doesNameExist) {
-    res.send({error: 'Nickname taken, please try another'});
+
+  if (!nameValid || doesNameExist) {
+    res.send({ error: 'Nickname taken, please try another' });
   } else {
-    res.send({name, error: false})
+    res.send({ name, error: false })
   }
 });
 
